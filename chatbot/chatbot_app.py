ce338dfac4f87478d47d3c4219a8d5f7acabe799
z3sh4n <ghaniwalamohmmad@gmail.com>
2025-10-11 19:27:59 +0530

    added streaming in the chatbot

--- chatbot/chatbot_app.py
+++ chatbot/chatbot_app.py
@@ -1,31 +1,42 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import streamlit as st
-from chatbot.chatbot_v1_2 import workflow, ChatState
+from chatbot.chatbot_app_backend import workflow, ChatState  
 from langchain_core.runnables.config import RunnableConfig
 from langchain_core.messages import HumanMessage
 
+# Initialize session state for messages if not present
 if 'messages' not in st.session_state:
-  st.session_state['messages'] = []
-  
+    st.session_state['messages'] = []
+
 CONFIG: RunnableConfig = {"configurable": {"thread_id": "1"}}
 
-for messages in st.session_state.messages:
-  with st.chat_message(messages["role"]):
-    st.text(messages["content"]) 
+# Display chat history
+for message in st.session_state['messages']:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
 
+# User input
 user_input = st.chat_input("Ask me anything")
 
 if user_input:
-  
-  st.session_state.messages.append({"role": "user", "content": user_input})
-  
-  init_state: ChatState = {'messages': [HumanMessage(user_input)]}
-  
-  final_state = workflow.invoke(init_state, config=CONFIG)
-  
-  with st.chat_message("user"):
-    st.text(user_input)
-  
-  with st.chat_message("assistant"):
-    st.text(final_state['messages'][-1].content)
-  
-  st.session_state.messages.append({"role": "assistant", "content": "Hello! How can I help you?"})
+    # Append user message to session state and display it
+    st.session_state['messages'].append({"role": "user", "content": user_input})
+    with st.chat_message("user"):
+        st.text(user_input)
+
+    # Prepare initial state for workflow
+    init_state: ChatState = {'messages': [HumanMessage(content=user_input)]}
+
+
+    # Display and append assistant response
+    with st.chat_message("assistant"):
+        assistant_response = st.write_stream(
+          message_chunk.content for message_chunk, metadata in workflow.stream(
+            init_state, 
+            config=CONFIG,
+            stream_mode='messages',
+            )
+          )
+    st.session_state['messages'].append({"role": "assistant", "content": assistant_response})
